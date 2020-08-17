46450ebf0588007001486c7bc798a71b1b2078c1
McFly91 <martin91200@gmail.com>
2020-08-17 11:34:07 +0200

    Création des routes

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const stuffRoutes = require("./routes/stuff");
-const userRoutes = require("./routes/user")
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -15,17 +15,17 @@ mongoose.connect('mongodb+srv://marty:mcfly@cluster0.uaio5.mongodb.net/Cluster0?
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 app.use(bodyParser.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use("/api/stuff", stuffRoutes);
+app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,20 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const stuffCtrl = require("../controllers/stuff");
+const sauceCtrl = require("../controllers/sauce");
 
 const auth = require("../middleware/auth");
 
 const multer = require("../middleware/multer-config")
 
-router.post('/', auth, multer, stuffCtrl.createThing);
+router.post("/", auth, multer, sauceCtrl.createSauce);
 
-router.put("/:id", auth, multer, stuffCtrl.modifyThing);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 
-router.delete("/:id", auth, stuffCtrl.deleteThing);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
-router.get("/:id", auth, stuffCtrl.getOneThing);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
 
-router.get('/', auth, stuffCtrl.getAllThings);
+router.get("/", auth, sauceCtrl.getAllSauces);
+
+router.post("/:id/like", auth, sauceCtrl.addLike)
 
 module.exports = router;
