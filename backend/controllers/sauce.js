86c5c444be237269c3fcfe53d6e6f26dcef390a6
McFly91 <martin91200@gmail.com>
2020-09-14 18:11:15 +0200

    Ajout d'un message d'erreur personnalisé en cas d'erreur ID

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -60,7 +60,7 @@ exports.deleteSauce = (req, res, next) => {
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id : req.params.id })
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(400).json({ error }))
+        .catch(() => res.status(400).json({ error : "Erreur ID dans l'URL : " + req.url}))
 };
 
 exports.getAllSauces = (req, res, next) => {
